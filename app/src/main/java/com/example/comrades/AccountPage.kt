85803eacdb6314bc72386c8137eb5d3a980ea181
rhanakeawe <rhanakeawe@gmail.com>
2024-10-02 16:34:39 -0700

    query in CalenderActivity

--- app/src/main/java/com/example/comrades/AccountPage.kt
+++ app/src/main/java/com/example/comrades/AccountPage.kt
@@ -2,8 +2,11 @@ package com.example.comrades
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
@@ -13,32 +16,21 @@ class AccountPage : Fragment(R.layout.fragment_account) {
     private lateinit var accountName : TextView
     private lateinit var desc : TextView
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_account, container, false)
 
         accountName = view.findViewById(R.id.accountName)
         desc = view.findViewById(R.id.desc)
-        val user = arguments?.getString("user")
-        val db = Firebase.firestore
-        val userRef = db.collection("users")
 
-        val query = userRef.whereEqualTo("userEmail",user)
+        accountName.text = arguments?.getString("userName")
+        desc.text = arguments?.getString("userDesc")
 
-        query.get()
-            .addOnSuccessListener { documents ->
-                Log.i("user","Got User!")
-                if (documents != null) {
-                    for (document in documents) {
-                        desc.text = document.data["userDesc"].toString()
-                        accountName.text = document.data["userName"].toString()
-                    }
-                } else {
-                    Log.w("user", "User null!")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w("user", "Error getting user!", exception)
-            }
+        return view
     }
 
 }
--- app/src/main/java/com/example/comrades/CalenderActivity.kt
+++ app/src/main/java/com/example/comrades/CalenderActivity.kt
@@ -3,6 +3,8 @@ package com.example.comrades
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentContainerView
@@ -10,6 +12,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
 
 class CalenderActivity : AppCompatActivity() {
 
@@ -33,7 +36,6 @@ class CalenderActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         auth = Firebase.auth
-        val user = auth.currentUser?.email
 
         bottomNavigationView = findViewById(R.id.bottomNavView)
         fragmentContainerView = findViewById(R.id.frag_containter)
@@ -42,8 +44,28 @@ class CalenderActivity : AppCompatActivity() {
         val eventsFragment = EventsList()
         val accountFragment = AccountPage()
 
+        val db = Firebase.firestore
+        val userRef = db.collection("users")
+        val query = userRef.whereEqualTo("userEmail", auth.currentUser?.email)
+
         val bundle = Bundle()
-        bundle.putString("user","$user")
+        bundle.putString("userEmail","${auth.currentUser?.email}")
+
+        query.get()
+            .addOnSuccessListener { documents ->
+                Log.i("user","Got User!")
+                if (documents != null) {
+                    for (document in documents) {
+                        bundle.putString("userDesc",document.data["userDesc"].toString())
+                        bundle.putString("userName", document.data["userName"].toString())
+                    }
+                } else {
+                    Log.w("user", "User null!")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("user", "Error getting user!", exception)
+            }
 
         createEventFragment.arguments = bundle
         eventsFragment.arguments = bundle
